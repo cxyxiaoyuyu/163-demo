5445f0563bc13502df2f1cd7afeab5ef96120cca
xiaoyu <923996381@qq.com>
2019-03-18 00:49:51 +0800

    add lyrics scroll

--- src/js/play/play-song.js
+++ src/js/play/play-song.js
@@ -8,17 +8,47 @@
 
         `,
         render(data){
-            console.log(data)
             this.$el.find('audio').attr('src',data.url)
             this.$el.find('.songImg').attr('src',data.img)
             this.$el.find('.cover').css('background-image',`url('${data.img}')`)
+            this.$el.find('.songName').text(data.name)
+            let lyrics = data.lyrics.split('\n')
+            lyrics.map((value)=>{
+                let reg = /\[([\d:.]+)\](.+)/
+                let matches =  value.match(reg)
+                let time = parseInt(matches[1].split(':')[0]) * 60 + parseFloat(matches[1].split(':')[1])
+                this.$el.find('.lyrics>.lines').append(`
+                    <p data-time="${time}">${matches[2]}</p>
+                `)
+            })
         },
         play(){
-            console.log(this.$el.find('audio'))
             this.$el.find('audio')[0].play()
         },
         pause(){
             this.$el.find('audio')[0].pause()
+        },
+        showLyrics(time){
+            let p
+            $.each(this.$el.find('.lyrics>.lines>p'),(index,value)=>{
+                if($(value).next().length){
+                    let currentTime = $(value).attr('data-time')
+                    let nextTime = $(value).next().attr('data-time')
+                    if(time>=currentTime && time <=nextTime){
+                        p = value
+                        return false
+                    }
+                }else{
+                    p = value
+                }
+            })
+            $(p).addClass('active').siblings().removeClass('active')
+            let pHeight = p.getBoundingClientRect().top
+            let linesHeight = this.$el.find('.lyrics>.lines')[0].getBoundingClientRect().top
+            let height = pHeight - linesHeight - 24
+            this.$el.find('.lines').css({
+                transform: `translateY(-${height}px)`
+            })
         },
     }
     let model = {
@@ -41,7 +71,6 @@
         },
         bindEvents(){
             this.view.$el.on('click','.disc-container',(ev)=>{
-                console.log('1111')
                 if($(ev.currentTarget).hasClass('playing')){
                     this.view.$el.find('#play').removeClass('hide')
                     this.view.pause()
@@ -51,14 +80,20 @@
                 }
                 $(ev.currentTarget).toggleClass('playing')
             })
+            let audio = this.view.$el.find('audio')[0]
+            audio.onended = ()=>{
+                this.view.pause()
+                this.view.$el.find('.disc-container').removeClass('playing')
+            }
+            audio.ontimeupdate = ()=>{
+                this.view.showLyrics(audio.currentTime)
+            }
         },
         getSongId(){
             let search = window.location.search.substring(1)
             let query = search.split('&').filter(v=>v)
             let id = query[0].split('=')[1]
-            console.log(id)
             this.model.get(id).then(()=>{
-                console.log(this.model.data)
                 this.view.render(this.model.data)
             })
         }
