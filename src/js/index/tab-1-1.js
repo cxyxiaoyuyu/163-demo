4c9197d456136d71ff9e1ab6139ab4a6d057bb75
xiaoyu <923996381@qq.com>
2019-03-18 23:53:35 +0800

    index page show playlist

--- src/js/index/tab-1-1.js
+++ src/js/index/tab-1-1.js
@@ -1,3 +1,58 @@
 {
-    console.log('1111111111111')
+    let view = {
+        el: '.songSheetList',
+        init(){
+            this.$el = $(this.el)
+        },
+        render(data={}){
+            console.log(data)
+            data.map((value)=>{
+                console.log(value)
+                this.$el.append(`
+                    <li class="songSheet" playlist-id="${value.id}">
+                        <div class="img">
+                            <img src="${value.img}">
+                            <p>${value.summary}</p>
+                        </div>
+                    </li>
+                `)
+            })
+        }
+    }
+
+    let model = {
+        data: {
+            playlists: []
+        },
+        find(){
+            var query = new AV.Query('playlist');
+            console.log(2)
+            console.log(query)
+            // top 6
+            return query.find().then((playlist)=>{
+                console.log(playlist)
+                this.data.playlists = playlist.map((playlist)=>{
+                    this.data.playlists.push({id:playlist.id,...playlist.attributes})
+                    console.log(this.data)
+                    return {id:playlist.id,...playlist.attributes}
+                })
+            })
+        }
+    }
+
+    let controller = {
+        init(view,model){
+            console.log(1)
+            this.view = view
+            this.view.init()
+            this.model = model
+            this.model.find().then(()=>{
+                console.log(this.model.data)
+                this.view.render(this.model.data.playlists)
+            })
+
+        },
+
+    }
+    controller.init(view,model)
 }
